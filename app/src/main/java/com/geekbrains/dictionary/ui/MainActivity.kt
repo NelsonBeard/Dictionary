3bf48e1fcd9ca06ba95dfb8d6e449f36dfa9e1ad
NelsonBeard <ilnur111111@yandex.ru>
2022-06-05 14:18:09 +0500

    Добавил создание VieModel через фабрику и сохранение загруженного списка переводов при повороте экрана

--- app/src/main/java/com/geekbrains/dictionary/ui/MainActivity.kt
+++ app/src/main/java/com/geekbrains/dictionary/ui/MainActivity.kt
@@ -4,15 +4,18 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import com.geekbrains.dictionary.app
+import com.geekbrains.dictionary.data.LocalRepo
 import com.geekbrains.dictionary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainViewModel
     private val adapter = DataFromServerListAdapter()
+
+    private val viewModel: MainViewModel by viewModels { MainViewModelFactory(app.translationRepo) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +23,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         binding.translationRecyclerView.adapter = adapter
+        restoreAdapter()
 
         binding.searchWordButton.setOnClickListener {
             val wordToSearch = binding.inputWordEditText.text.toString()
 
             showTranslation(wordToSearch)
             hideKeyboard(currentFocus ?: View(this))
+        }
+    }
+
+    private fun restoreAdapter() {
+        val savedData = LocalRepo.getData()
+
+        if (savedData != null) {
+            adapter.setData(savedData)
         }
     }
 
--- app/src/main/java/com/geekbrains/dictionary/ui/MainViewModel.kt
+++ app/src/main/java/com/geekbrains/dictionary/ui/MainViewModel.kt
@@ -3,16 +3,17 @@ package com.geekbrains.dictionary.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.geekbrains.dictionary.data.RetrofitImplementation
+import com.geekbrains.dictionary.data.LocalRepo
 import com.geekbrains.dictionary.domain.entity.DataFromServer
+import com.geekbrains.dictionary.domain.usecase.TranslationRepo
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.subscribeBy
 
-
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val translationRepo: TranslationRepo
+) : ViewModel() {
 
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
-    private val translationRepo = RetrofitImplementation()
 
     private val _liveDataFromServer = MutableLiveData<List<DataFromServer>>()
     val liveDataFromServer: LiveData<List<DataFromServer>> = _liveDataFromServer
@@ -21,8 +22,15 @@ class MainViewModel : ViewModel() {
         compositeDisposable.add(
             translationRepo
                 .getTranslation(word)
-                .subscribeBy { _liveDataFromServer.postValue(it) }
+                .subscribeBy {
+                    _liveDataFromServer.postValue(it)
+                    saveToLocalRepo(it)
+                }
         )
+    }
+
+    private fun saveToLocalRepo(data: List<DataFromServer>) {
+        LocalRepo.saveData(data)
     }
 
     override fun onCleared() {
